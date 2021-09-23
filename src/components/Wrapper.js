537d43341755bd7f18c8d3c78559632117823624
Zdobnov <dr.ankh@gmail.com>
2021-09-23 20:22:31 +0200

    wrapper moved to scss approach

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,33 +1,16 @@
 import classnames from 'classnames';
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
-  wrapper: {
-    width: '100%',
-    maxWidth: '1350px',
-    margin: '0 auto',
-    padding: '0 20px',
+import './Wrapper.scss';
 
-    '@media screen and (min-width: 992px)': {
-      paddingLeft: '30px',
-      paddingRight: '30px',
-    },
-  },
-}));
-
-const Wrapper = ({ children, className }) => {
-  const styles = useStyles();
-
-  return (
-    <div
-      className={classnames(styles.wrapper, {
-        [className]: className,
-      })}
-    >
-      {children}
-    </div>
-  );
-}
+const Wrapper = ({ children, className }) => (
+  <div
+    className={classnames('wrapper', {
+      [className]: className,
+    })}
+  >
+    {children}
+  </div>
+);
 
 export default Wrapper;
