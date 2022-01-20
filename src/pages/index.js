f3b41cdf2a136f1fdcfb5ffff601752d9c7ebef6
Zdobnov <dr.ankh@gmail.com>
2022-01-20 22:41:10 +0100

    helmet code deleted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
 
 import DrivingSchool from '../components/DrivingSchool/DrivingSchool';
 import Exam from '../components/Exam';
@@ -21,12 +20,6 @@ import '../styles/index.scss';
 
 const IndexPage = ({ data: { home } }) => (
   <Fragment>
-    {/*<Helmet>*/}
-    {/*  <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png"/>*/}
-    {/*  <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/favicon-32x32.png"/>*/}
-    {/*  <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/favicon-16x16.png"/>*/}
-    {/*</Helmet>*/}
-
     <LanguageContextProvider>
       <Header/>
       <Promo/>
