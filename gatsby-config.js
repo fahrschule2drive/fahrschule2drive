dfc912be49a4fb861e1f56b4b00c6bb5d3cda403
Zdobnov <dr.ankh@gmail.com>
2022-01-20 21:46:00 +0100

    favicon fixed again

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,22 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: 'gatsby-plugin-favicons',
+      options: {
+        logo: './src/images/favicon/favicon.ico',
+        appName: 'Drive 2 Fahrschule',
+        background: '#fff',
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          yandex: false,
+          windows: false
+        },
+      },
+    },
   ],
 }
