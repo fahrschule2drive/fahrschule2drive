fb1d09dd1568615061fba659505cdcaf80c45e4f
Zdobnov <dr.ankh@gmail.com>
2022-01-20 22:31:41 +0100

    favicon, gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,17 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon/favicon-32x32.png`,
+      },
+    },
   ],
 }
