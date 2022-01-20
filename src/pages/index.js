0a582a25be9a1951af63d0b0ae90ec87711ed695
Zdobnov <dr.ankh@gmail.com>
2022-01-20 21:15:10 +0100

    favicon fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,9 @@ import '../styles/index.scss';
 const IndexPage = ({ data: { home } }) => (
   <Fragment>
     <Helmet>
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/favicon-16x16.png"/>
     </Helmet>
 
     <LanguageContextProvider>
--- src/pages/policy.js
+++ src/pages/policy.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 
 import Article from '../components/Article';
 import Footer from '../components/Footer/Footer';
@@ -14,6 +15,12 @@ const DataPolicy = () => (
       datoCmsPolicyPage: policy
     }) => (
       <Fragment>
+        <Helmet>
+          <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/favicon-16x16.png"/>
+        </Helmet>
+
         <Header type="dark"/>
         <Article
           content={policy.content}
